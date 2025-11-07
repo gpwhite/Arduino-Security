d87f9dd1ae7c97ea7bc73b1656ca7be7d70ef0e6
GPWhite <gpwhite@buffalo.edu>
2025-11-07 15:57:39 -0500

    yipee

--- sketch_nov6a/sketch_nov6a.ino
+++ sketch_nov6a/sketch_nov6a.ino
@@ -1,11 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
 pinMode(7,OUTPUT);
+pinMode(6,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(7,HIGH);
-//blinky works
 
+if(analogRead(0)>100){
+  digitalWrite(7,HIGH);
+  delay(200);
+  digitalWrite(7,LOW);
+//Door sensor works, please dont do what I did and assume its a knock sensor and slam it up and down like a fucking moron. 
+
+}
 }
