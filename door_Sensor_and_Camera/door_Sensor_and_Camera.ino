95284b46e4b0316c6153fb747932e7ee937121d8
GPWhite <gpwhite@buffalo.edu>
2025-11-20 02:17:15 -0500

    aaaahhhhh

--- door_Sensor_and_Camera/door_Sensor_and_Camera.ino
+++ door_Sensor_and_Camera/door_Sensor_and_Camera.ino
@@ -1,25 +1,27 @@
+//----------------------------------------------------------------------
+//--------------------------------PIN LAYOUT----------------------------
+// Pin 7: Input_Pullup for door sensor, other line connects to GND
+// Pin 6: Speaker output
+//----------------------------------------------------------------------
+  
 void setup() {
-  // put your setup code here, to run once:
-
-  //-----------------------------------------------------------------------------------------------------------
-  //--------------------------------PIN LAYOUT----------------------------
-  //
 pinMode(7,INPUT_PULLUP); //INIT Door sensor input
-
+pinMode(6,OUTPUT);
 Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-if(digitalRead(7)==LOW){
-  Serial.print("detected closed\n");
-  //delay(200);
-  //digitalWrite(7,LOW);
-//Door sensor works, please dont do what I did and assume its a knock sensor and slam it up and down like a fucking moron. 
-
-}
+  //Door Detection Code, outputs to serial monitor----------------------
+  if(digitalRead(7)==LOW){
+     Serial.print("detected closed\n");
+     
+  }
 else{
-  Serial.print("Open\n");
+  digitalWrite(6,HIGH);
 }
+//END Door Code---------------------------------------------------------
+//BEGIN Camera Code
+
+
 }
--- sketch_nov6a/sketch_nov6a.ino
+++ sketch_nov6a/sketch_nov6a.ino
@@ -1,17 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
-pinMode(7,OUTPUT);
-pinMode(6,INPUT);
+
+  //-----------------------------------------------------------------------------------------------------------
+  //--------------------------------PIN LAYOUT----------------------------
+  //
+pinMode(7,INPUT_PULLUP); //INIT Door sensor input
+
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-if(analogRead(0)>100){
-  digitalWrite(7,HIGH);
-  delay(200);
-  digitalWrite(7,LOW);
+if(digitalRead(7)==LOW){
+  Serial.print("detected closed\n");
+  //delay(200);
+  //digitalWrite(7,LOW);
 //Door sensor works, please dont do what I did and assume its a knock sensor and slam it up and down like a fucking moron. 
 
+}
+else{
+  Serial.print("Open\n");
 }
 }
